33b2e08f5829e339adf1b21ce4784d70489865e1
Maciej Kaczmarczyk <maciej.kaczmarczyk@likims.com>
2021-01-16 09:38:36 +0100

    chore(linters): check before push

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 
