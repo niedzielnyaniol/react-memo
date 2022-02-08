5b92e8b4de35b468c0cc93f0788befb809cac785
Maciej Kaczmarczyk <maciej.kaczmarczyk@grassvalley.com>
2022-02-08 19:28:24 +0100

    chore: add eslint-config-niedzielnyaniol

--- src/config/BoardSizes.ts
+++ src/config/BoardSizes.ts
@@ -5,7 +5,7 @@ type BoardSizesConfig = { [key in BoardSize]: { cols: number; rows: number } };
 const BOARD_SIZES: BoardSizesConfig = {
   small: { rows: 4, cols: 2 },
   medium: { rows: 4, cols: 3 },
-  large: { rows: 4, cols: 4 },
+  large: { rows: 7, cols: 7 },
 };
 
 export { BOARD_SIZES };
